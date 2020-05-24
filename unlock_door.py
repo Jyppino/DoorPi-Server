c756f04950f397e8b8b795e47b0277fa410e3660
Jaap <jaapetto@gmail.com>
2020-05-24 20:38:22 +0200

    Unlock time now 1 second

--- unlock_door.py
+++ unlock_door.py
@@ -3,42 +3,47 @@ import RPi.GPIO as GPIO
 import time
 
 # Config
-RelayPin = 20 # Relay CH2
-SecondsWait = 2
+RelayPin = 20  # Relay CH2
+SecondsWait = 1
 
 # Setup function
+
+
 def setup():
     GPIO.setwarnings(False)
     # Set the GPIO modes to BCM numbering
     GPIO.setmode(GPIO.BCM)
     # Set RelayPin's mode to output,and initial level to LOW(0V)
-    GPIO.setup(RelayPin,GPIO.OUT,initial=GPIO.HIGH)
+    GPIO.setup(RelayPin, GPIO.OUT, initial=GPIO.HIGH)
 
 # Main function
-def main():        
+
+
+def main():
     # Low output = open relay
     print('Doorlock open..')
-    GPIO.output(RelayPin,GPIO.LOW)
-    # Wait X seconds 
+    GPIO.output(RelayPin, GPIO.LOW)
+    # Wait X seconds
     time.sleep(SecondsWait)
     # High output = closed relay
     print('Doorlock neutral..')
-    GPIO.output(RelayPin,GPIO.HIGH)
+    GPIO.output(RelayPin, GPIO.HIGH)
     # Cleanup
     destroy()
 
 # Define a destroy function for clean up everything after the script finished
+
+
 def destroy():
     # Turn off relay
-    GPIO.output(RelayPin,GPIO.LOW)
+    GPIO.output(RelayPin, GPIO.LOW)
     # Release resource
     GPIO.cleanup()
+
 
 if __name__ == '__main__':
     setup()
     try:
         main()
-    except KeyboardInterrupt: # Ctrl+C
+    except KeyboardInterrupt:  # Ctrl+C
         destroy()
-
-    
